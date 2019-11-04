2ffb58558a5297b046d7e8eb31e3567dbe2b44ba
parinaalamir <44803911+parinaalamir@users.noreply.github.com>
2019-11-04 17:12:33 +0330

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,10 @@
+var ChatZone = document.getElementById('chat-area');
+var count = 0;
+var MCounter = 0;
+var UCounter = 0;
+var LastMessage = {
+    message: "",
+    sender: ""
+};
+var SendChat = [false, ""];
+var BecomeSilent = false;
