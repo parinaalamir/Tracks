2af537a554a557dd4f6ce8773a3dfffa0fae66e4
parinaalamir <44803911+parinaalamir@users.noreply.github.com>
2019-11-04 17:10:07 +0330

    Create Engine.js

---
+++ Engine.js
@@ -0,0 +1,35 @@
+function find() {
+    if (count == 0) {
+        StartChat();
+    }
+
+    console.log("waiting for chat " + (count + 1));
+    last = LastMessage.sender;
+
+    if (last == "bot") {
+        User();
+    } else {
+        Marker.focus();
+        if (BecomeSilent) {
+            LastMessage.sender = "bot";
+            LastMessage.message = "";
+            waiting();
+        } else {
+            if (count == 0) {
+                wait = 100;
+            } else {
+                wait = 500;
+            }
+            setTimeout(function(){
+                console.log("Waiting");
+                ChatBot();
+            }, wait);
+        }
+    }
+}
+
+function StartChat(){
+    console.log("Starting Chat Bot...");
+    ChatZone.innerHTML = '';
+    document.getElementById('compose-area').style.display = 'block';
+}
