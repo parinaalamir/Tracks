10d3e3355251ef904453c84e8f6f8d32939d38a4
parinaalamir <44803911+parinaalamir@users.noreply.github.com>
2019-11-04 17:08:48 +0330

    Create BotMessages.js

---
+++ BotMessages.js
@@ -0,0 +1,8 @@
+var Messages = [
+    "Hi! I am going to ask your specification. Please enter your fleet name.",
+    "please enter your name.",
+    "please enter your number.",
+    "please enter your truck id."
+];
+
+var end = "This information will be save.";
