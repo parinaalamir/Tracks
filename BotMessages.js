a243322c2db4b848011559b398ec527f80201eae
parinaalamir <44803911+parinaalamir@users.noreply.github.com>
2019-11-06 17:15:17 +0330

    Add files via upload

---
+++ BotMessages.js
@@ -0,0 +1,15 @@
+var Messages = [
+    "Hallow, what's your name?",
+    "Hi, name what's the name of your company?",
+    "Do you own trucks?",
+    "How many trucks do you have?",
+    "What brands are they",
+    "How many truckname trucks do you have?",
+    "Are they of the same model?",
+    "What model are they?",
+    "What is engine size?",
+    "How many axles do they have?",
+    "How many such trucks do you have?"
+];
+
+var end = "This conversation will be save.";
